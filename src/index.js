fba48ade6704aa9c015b51338e4a8f0c270aa92f
Alia Wilkinson <aliawilkinson@gmail.com>
2018-04-25 22:32:50 -0700

    prototype complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+    greeting() {
+        return (<div className="container">
+            <h1>Hello {this.name}</h1>
+            <h2 className="text-muted">Your lucky number is {this.luckyNumber()}</h2>
+        </div>)
+    }
+    luckyNumber() {
+        return Math.floor(Math.random() * 1000) + 1
+    }
+}
+
+const Harrison = new User('Harrison');
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    Harrison.greeting(),
     document.getElementById('root')
 );
