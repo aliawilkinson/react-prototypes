f1ca2cbb7b3eca16366415116254391da067fba1
Alia Wilkinson <aliawilkinson@gmail.com>
2018-05-01 01:23:46 -0700

    working version, up to feature set 4

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import ContactList from './contact_list';
 
 export default () => {
     return (
         <div className="container">
             <h1 className="text-center">Address Book</h1>
+            <ContactList />
         </div>
     )
 }
