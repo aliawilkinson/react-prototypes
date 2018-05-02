1cad47ac28d52269b55508f16bfeb4fe43cc2c47
Alia Wilkinson <aliawilkinson@gmail.com>
2018-05-01 17:47:32 -0700

    feature set 4, actually working

--- src/components/contact_form.js
+++ src/components/contact_form.js
@@ -5,44 +5,40 @@ import Field from './field.js'
 class ContactForm extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
-            form: {
-                firstName: '',
-                lastName: '',
-                phone: '',
-                email: ''
-            }
-        }
-
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+            firstName: '',
+            lastName: '',
+            phone: '',
+            email: ''
+        };
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
-
     handleInputChange(event) {
-        const { value, name } = event.target;
-        const { form } = this.state;
-
-        this.state[name] = value;
-        console.log(this.state);
-
+        const { name, value } = event.target;
+        console.log('NAME: %s- VALUE: %s', name, value);
         this.setState({
-            form: { ...form }
-        });
+            [name]: value
+        })
     }
 
     handleSubmit() {
         event.preventDefault();
-        console.log('handleSubmit called, form values are:', this.state.form);
+        console.log('handleSubmit called, form values are:', this.state);
     }
 
     render() {
-        const { firstName, lastName } = this.state;
+        const { firstName, lastName, phone, email } = this.state;
         return (
-            <form onSubmit={this.handleSubmit}>
-                <Field name="firstName" label="FirstName" type="text" value={firstName} onChange={this.handleInputChange} />
-                <button>AddContact</button>
-            </form>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <Field name="firstName" label="FirstName" type="text" value={firstName} onChange={this.handleInputChange} />
+                    <Field name="lastName" label="LastName" type="text" value={lastName} onChange={this.handleInputChange} />
+                    <Field name="phone" label="Phone" type="text" value={phone} onChange={this.handleInputChange} />
+                    <Field name="email" label="Email" type="text" value={email} onChange={this.handleInputChange} />
+                </form>
+                <button onClick={this.handleSubmit}>AddContact</button>
+            </div>
         )
     }
 }
@@ -64,3 +60,56 @@ export default ContactForm;
             //         <input onChange={this.handleInputChange} value={lastName} name="lastName" type="text" className="form-control" />
             //     </div>
             // </form> */
+
+//             import React, {Component} from 'react';
+// import Field from './field'
+// class ContactForm extends Component{
+//    constructor(props){
+//        super(props);
+//        this.state={
+//            firstName:'',
+//            lastName:'',
+//            phone:'',
+//            email:''
+//        };
+//        this.handleInputChange = this.handleInputChange.bind(this)
+//        this.handleSubmit=this.handleSubmit.bind(this)
+//    }
+//    handleInputChange(event){
+//        const { name, value }= event.target;
+//        console.log('NAME: %s- VALUE: %s', name,value);
+//        this.setState({
+//            [name]:value
+//        })
+//    }
+//    handleSubmit(event){
+//        event.preventDefault();
+//        this.props.add(this.state);
+//        console.log('handleSubmit Created : ', this.state);
+//        const newState= {
+//            firstName:'',
+//            lastName:'',
+//            phone:'',
+//            email:''
+//        };
+//        this.setState(newState);
+//    }
+//    render (){
+//        const { firstName,lastName, phone, email } =this.state;
+//        return(
+//            <form className='form' onSubmit={this.handleSubmit}>
+//                {/* <div className="form-group">
+//                    <label>First Name</label>
+//                    <input onChange={this.handleInputChange} value={firstName} name="firstName" type="text" className="form-control"/>
+//                </div> */}
+//                <Field name="firstName" label="First Name" type="text" value={firstName} onChange={this.handleInputChange}/>
+//                <Field name="lastName" label="Last Name" type="text" value={lastName} onChange={this.handleInputChange}/>
+//                <Field name="phone" label="Phone Number" type="tel" value={phone} onChange={this.handleInputChange}/>
+//                <Field name="email" label="email" type="email" value={email} onChange={this.handleInputChange}/>
+
+//                <button> Add Contact </button>
+//            </form>
+//        )
+//    }
+// }
+// export default ContactForm
--- src/components/field.js
+++ src/components/field.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default props => {
-    console.log(props)
+    // console.log(props)
     return (
         <div className="form-group">
             <label>{props.label}</label>
