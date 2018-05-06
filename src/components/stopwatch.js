68ae8d9576f2909a943fcd1907f0ef6bf324142b
Alia Wilkinson <aliawilkinson@gmail.com>
2018-05-06 16:41:38 -0700

    mvp + feature sets 6, 7 on clock

--- src/components/stopwatch.js
+++ src/components/stopwatch.js
@@ -1,20 +1,79 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import Time from './format_time';
 
 class Stopwatch extends Component {
+
     constructor(props) {
         super(props);
 
         this.state = {
-            time: new Date()
+            status: 'stopped',
+            start: null,
+            elapsed: 0
+        };
+
+        this.start = this.start.bind(this);
+        this.stop = this.stop.bind(this);
+        this.update = this.update.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+
+    start() {
+        const { start, elapsed } = this.state;
+        var newStart = new Date().getTime();
+        if (start) {
+            newStart -= elapsed;
+        }
+        this.setState({
+            status: 'running',
+            start: newStart
+        });
+
+        setTimeout(this.update, 10);
+    }
+
+    stop() {
+        this.setState({
+            status: 'stopped'
+        });
+    }
+
+    reset() {
+        this.setState({
+            status: 'stopped',
+            start: null,
+            elapsed: 0
+        });
+    }
+
+    update() {
+        const { status, start } = this.state;
+
+        if (status === 'running') {
+            this.setState({
+                elapsed: new Date().getTime() - start
+            });
+
+            setTimeout(this.update, 10);
         }
     }
+
     render() {
+        const { elapsed, status } = this.state;
+
         return (
-            <div>This is a text stopwatch</div>
-        );
+            <div className="jumbotron">
+                <h1 className="display-3"><Time elapsed={elapsed} /></h1>
+                <hr className="my-3"></hr>
+                <p className="lead text-center">{status}</p>
+                <p className="text-center">
+                    <button className="btn btn-outline-success mx3" onClick={this.start}>Start</button>
+                    <button className="btn btn-outline-danger mx3" onClick={this.stop}>Stop</button>
+                    <button className="btn btn-outline-warning mx3" onClick={this.reset}>Reset</button>
+                </p>
+            </div>
+        )
     }
 }
 
 export default Stopwatch;
-
